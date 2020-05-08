a6bd5ea59548f86bd4c18dee969e3e4351aca7da
Christoffer <christoffer.bohlin@hotmail.com>
2020-05-08 13:34:09 +0100

    introduction videos completed

---
+++ router/300_movie.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Create routes using $app programming style.
+ */
+//var_dump(array_keys(get_defined_vars()));
+
+/**
+ * Show all movies.
+ */
+$app->router->get("movie", function () use ($app) {
+    $title = "Movie database | oophp";
+
+    // $title = "Show all movies";
+    // $view[] = "view/show-all.php";
+    // $sql = "SELECT * FROM movie;";
+    // $resultset = $db->executeFetchAll($sql);
+
+
+    $app->db->connect();
+    $sql = "SELECT * FROM movie;";
+    $res = $app->db->executeFetchAll($sql);
+
+    $app->page->add("movie/index", [
+        "resultset" => $res,
+    ]);
+
+    return $app->page->render([
+        "title" => $title,
+    ]);
+});
